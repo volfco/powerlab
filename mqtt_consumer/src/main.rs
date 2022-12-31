cba1aa2a8aec0c0627cd5634ee284cad35aa7e1c
Colum <git@volf.co>
2022-12-31 12:31:37 -0800

    Convert to using BB8 for Postgres

--- mqtt_consumer/src/main.rs
+++ mqtt_consumer/src/main.rs
@@ -1,8 +1,14 @@
+mod schema_guesser;
+
 use clap::Parser;
-use log::{debug, error, info, trace};
+use log::{debug, error, info, trace, warn};
 use rumqttc::{AsyncClient, MqttOptions, QoS};
+use std::str::FromStr;
 use std::time::Duration;
 use tokio::sync::mpsc::{channel, Receiver, Sender};
+
+use bb8::Pool;
+use bb8_postgres::PostgresConnectionManager;
 use tokio_postgres::NoTls;
 
 type DbChannelType = (String, serde_json::Value);
@@ -34,6 +40,30 @@ struct Args {
     )]
     db: String,
 
+    /// Attempt to guess the schema of the incoming data for topics in which the associated table
+    /// does not exist.
+    ///
+    /// If the topic `foo` is provided, but the `foo` table does not exist, the schema will be guessed
+    /// based off the first message this consumer receives.
+    ///
+    /// If the first message from the `foo` topic is:
+    ///     {"bar": 1, "baz": 2}
+    /// The resulting table will look like:
+    ///    CREATE TABLE foo (timestamp DATETIME, bar int, baz int);
+    ///
+    /// If the topic `foo` is provided, and the `foo` table exists, the schema will not be guessed.
+    #[clap(long)]
+    db_guess_schema: bool,
+
+    /// If guessing the schema, convert the generated table to a Timeseries table.
+    ///
+    /// Options are:
+    ///   - hypertable: Create a hypertable (TimeseriesDB)
+    ///
+    /// If not provided, no conversion will be performed and a standard table will be used.
+    #[clap(long)]
+    db_guess_schema_timeseries: Option<String>,
+
     /// Topics to subscribe to. Each argument is a comma separated map of topic to table.
     /// Example: iot-devices/solar,solar-power will consume the topic iot-devices/solar and write
     /// all messages to the table solar-power.
@@ -54,14 +84,104 @@ fn parse_topics(topics: Vec<String>) -> Vec<(String, String)> {
         .collect()
 }
 
+/// Check if the table
+async fn check_topic_table(pool: Pool<PostgresConnectionManager<NoTls>>, table: &String) -> bool {
+    let conn = pool.get().await.unwrap();
+    let select = conn
+        .prepare(
+            "SELECT EXISTS (
+    SELECT FROM 
+        pg_tables
+    WHERE 
+        schemaname = 'public' AND 
+        tablename  = '$1'
+    );",
+        )
+        .await
+        .unwrap();
+    let row = conn.query_one(&select, &[&table]).await.unwrap();
+    row.get::<usize, bool>(0)
+}
+
+async fn message_handler(
+    pool: Pool<PostgresConnectionManager<NoTls>>,
+    topic: String,
+    payload: serde_json::Value,
+) {
+    let conn = pool.get().await.unwrap();
+    trace!("Received message on topic {topic}");
+
+    let payload = payload.as_object().unwrap();
+    let keys = payload
+        .keys()
+        .map(|k| k.to_string())
+        .collect::<Vec<String>>();
+    let values = payload
+        .values()
+        .map(|v| v.to_string())
+        .collect::<Vec<String>>();
+
+    let table = topic.replace('/', "_");
+
+    let mut var_string = String::new();
+    for i in 1..keys.len() {
+        var_string.push_str(&format!("${i}, "));
+    }
+
+    let stmt_sql = format!(
+        "INSERT INTO {} (timestamp, {}) VALUES (NOW(), {})",
+        &table,
+        &keys.join(", "),
+        &var_string
+    );
+
+    debug!("Executing Prepared Statement SQL: {}", stmt_sql);
+
+    let stmt = conn.prepare(&stmt_sql).await.unwrap();
+    conn.execute_raw(&stmt, values).await.unwrap();
+}
+
+/// Comsume messages from the MQTT broker channel and write them to the database.
+async fn message_consumer(
+    pool: Pool<PostgresConnectionManager<NoTls>>,
+    mut rx: Receiver<DbChannelType>,
+    topic_map: Vec<(String, String)>,
+) {
+    let mut seen_tables = vec![];
+    while let Some((topic, payload)) = rx.recv().await {
+        if !seen_tables.contains(&topic) {
+            warn!("Topic {topic} has not been seen before. Checking if table exists.");
+            if check_topic_table(pool.clone(), &topic).await {
+                info!("Table {topic} exists. Continuing.");
+                seen_tables.push(topic.clone());
+            } else {
+                warn!("Table {topic} does not exist. Attempting to guess schema.");
+                let schema = schema_guesser::generate_schema_sql(&topic, &payload);
+                let conn = pool.get().await.unwrap();
+                if let Err(e) = conn.execute(&schema, &[]).await {
+                    error!("Failed to create table for topic {}: {}", &topic, e);
+                    continue;
+                }
+                seen_tables.push(topic.clone());
+                info!("Table {topic} created. Continuing.");
+            }
+        }
+
+        message_handler(pool.clone(), topic, payload).await;
+    }
+}
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     env_logger::init();
     let args = Args::parse();
 
     // connect to postgres
-    let (db_client, _) = tokio_postgres::connect(&args.db, NoTls).await?;
-    info!("connected to postgres");
+    let config = tokio_postgres::config::Config::from_str(
+        "postgresql://postgres:mysecretpassword@localhost:5432",
+    )?;
+    let manager = PostgresConnectionManager::new(config, NoTls);
+    let pool = Pool::builder().build(manager).await?;
 
     // connect to the mqtt broker
     let mut mqttoptions = MqttOptions::new(args.mqtt_id, args.mqtt_broker, args.mqtt_port);
@@ -70,53 +190,17 @@ async fn main() -> anyhow::Result<()> {
     let (client, mut eventloop) = AsyncClient::new(mqttoptions, 10);
     info!("connected to mqtt broker");
 
-    // Start database writer routine
-    let (db_tx, mut db_rx): (Sender<DbChannelType>, Receiver<DbChannelType>) = channel(32);
-
-    tokio::spawn(async move {
-        while let Some((topic, payload)) = db_rx.recv().await {
-            // unwrap payload and get the keys, values and convert to an insert statement
-            let payload = payload.as_object().unwrap();
-            let keys = payload
-                .keys()
-                .map(|k| k.to_string())
-                .collect::<Vec<String>>();
-            let values = payload
-                .values()
-                .map(|v| v.to_string())
-                .collect::<Vec<String>>();
-
-            let table = topic.replace('/', "_");
-
-            let mut var_string = String::new();
-            for i in 1..keys.len() {
-                var_string.push_str(&format!("${i}, "));
-            }
-
-            let stmt_sql = format!(
-                "INSERT INTO {} (timestamp, {}) VALUES (NOW(), {})",
-                &table,
-                &keys.join(", "),
-                &var_string
-            );
-
-            debug!("Executing Prepared Statement SQL: {}", stmt_sql);
-
-            let stmt = db_client.prepare(&stmt_sql).await.unwrap();
-
-            db_client.execute_raw(&stmt, values).await.unwrap();
-        }
-    });
-
-    // parse the topics into a vector of tuples that we can use
-    let topics = parse_topics(args.topics);
-    info!("parsed topics: {:?}", topics);
-
     // subscribe to all the given topics
-    for (topic, _) in topics.iter() {
-        debug!("Subscribing to topic: {}", topic);
+    let topic_map = parse_topics(args.topics);
+    for (topic, _) in topic_map.iter() {
         client.subscribe(topic, QoS::AtLeastOnce).await?;
+        debug!("subscribed to topic {topic}");
     }
+
+    // Start database writer routine
+    let (db_tx, db_rx): (Sender<DbChannelType>, Receiver<DbChannelType>) = channel(32);
+    let consumer_pool = pool.clone();
+    tokio::spawn(async move { message_consumer(consumer_pool, db_rx, topic_map).await });
 
     // now, wait for mqtt messages to be recieved and when they are- push them to the database writer channel
     loop {
---
+++ mqtt_consumer/src/schema_guesser.rs
@@ -0,0 +1,71 @@
+use serde_json::Value;
+use std::collections::{BTreeMap, HashMap};
+
+enum TypeMappings {
+    String,
+    Float,
+    Boolean,
+    Json,
+}
+
+pub fn generate_schema_sql(table_name: &String, input: &serde_json::Value) -> String {
+    if !input.is_object() {
+        todo!("handle non-object input")
+    }
+
+    // loop over each value in the object and get the type
+    let mut schema = BTreeMap::new();
+    for (key, value) in input.as_object().unwrap().iter() {
+        let type_mapping = match value {
+            serde_json::Value::String(_) => TypeMappings::String,
+            serde_json::Value::Number(_) => TypeMappings::Float,
+            serde_json::Value::Bool(_) => TypeMappings::Boolean,
+            serde_json::Value::Array(_) => TypeMappings::Json,
+            serde_json::Value::Object(_) => TypeMappings::Json,
+            serde_json::Value::Null => TypeMappings::Json,
+        };
+
+        schema.insert(key.to_string(), type_mapping);
+    }
+
+    // now generate the sql
+    let mut sql = String::new();
+    for (key, value) in schema.iter() {
+        let type_mapping = match value {
+            TypeMappings::String => "TEXT",
+            TypeMappings::Float => "FLOAT",
+            TypeMappings::Boolean => "BOOLEAN",
+            TypeMappings::Json => "JSON",
+        };
+
+        sql.push_str(&format!("    {key} {type_mapping}, \n"));
+    }
+
+    let stmt = format!(
+        r"CREATE TABLE IF NOT EXISTS {table_name} (
+    timestamp TIMESTAMP NOT NULL,
+{sql});
+CREATE INDEX ix_{table_name}_time ON {table_name} (timestamp);"
+    );
+
+    stmt
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::schema_guesser::generate_schema_sql;
+
+    #[test]
+    fn test_basic_schema() {
+        let json_obj: serde_json::Value = serde_json::from_str(r#"{"foo": 1, "bar": 2}"#).unwrap();
+        let sql = generate_schema_sql(&"test_table".to_string(), &json_obj);
+
+        let expected = r#"CREATE TABLE IF NOT EXISTS test_table (
+    timestamp TIMESTAMP NOT NULL,
+    bar FLOAT, 
+    foo FLOAT, 
+);
+CREATE INDEX ix_test_table_time ON test_table (timestamp);"#;
+        assert_eq!(sql, expected);
+    }
+}

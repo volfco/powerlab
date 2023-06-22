e5b8d3f9179f4196ff465e9b306e8ac55557a998
Colum McGaley <colum.mcgaley@fastmail.com>
2023-06-21 21:05:58 -0700

    Refactor it a bit

--- victron_solar_monitor/src/main.rs
+++ victron_solar_monitor/src/main.rs
@@ -8,6 +8,7 @@ use std::thread;
 use std::time::Duration;
 use vedirect::{Events, VEError, MPPT};
 
+/// Read data from a Victron MPPT solar charge controller via a VE.Direct serial connection and publish it to an MQTT broker.
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
 struct Args {
@@ -17,32 +18,23 @@ struct Args {
 
     /// MQTT broker address
     #[clap(short, long)]
-    mqtt_broker: String,
+    host: String,
 
     /// MQTT Broker port
-    #[clap(short, long, default_value = "1883")]
-    mqtt_port: u16,
+    #[clap(short, long)]
+    port: u16,
 
-    /// MQTT Broker ID
-    #[clap(short, long, default_value = "victron_solar_monitor")]
-    mqtt_id: String,
-
-    /// MQTT Broker topic
-    #[clap(short, long, default_value = "victron_solar_monitor")]
-    mqtt_topic: String,
-
-    /// MQTT Broker Keep Alive
-    #[clap(short, long, default_value = "60")]
-    mqtt_keep_alive: u16,
+    /// MQTT topic
+    #[clap(short, long)]
+    topic: String,
 }
 
 struct Listener {
     sink_chan: mpsc::Sender<MPPT>,
 }
-
 impl Events<MPPT> for Listener {
     fn on_complete_block(&mut self, block: MPPT) {
-        trace!("read data {:#?}", &block);
+        trace!("read data {:?}", &block);
         if let Err(err) = self.sink_chan.send(block) {
             error!("unable to send block to channel. {:?}", err);
         }
@@ -63,10 +55,18 @@ impl Events<MPPT> for Listener {
 
 fn mqtt_publisher(topic: String, mut client: Client, rx: Receiver<MPPT>) {
     trace!("mqtt publisher thread started");
+    let mut count = 0;
     loop {
         let block = rx.recv().unwrap();
         let payload = serde_json::to_string(&block).unwrap();
         trace!("published to topic '{}'. payload : {:?}", &topic, &payload);
+
+        count += 1;
+        if count % 10 == 0 {
+            info!("{}", &payload);
+            count = 0;
+        }
+
         if let Err(e) = client.publish(&topic, QoS::AtLeastOnce, false, payload) {
             error!("unable to publish to topic '{}'. {:?}", &topic, e);
         }
@@ -79,19 +79,20 @@ fn main() -> anyhow::Result<()> {
     let args = Args::parse();
     debug!("parsed args: {:#?}", &args);
 
+    // open serial port
+    debug!("opening serial port: {}", args.device);
+
     let mut port = serialport::new(args.device.clone(), 19_200)
         .data_bits(serialport::DataBits::Eight)
         .timeout(core::time::Duration::from_secs(2))
         .open()
         .context("Failed to open vedirect serial port")?;
+
     info!("successfully opened serial port: {}", args.device);
 
     // connect to mqtt broker
-    let mut mqttoptions = MqttOptions::new(args.mqtt_id, args.mqtt_broker, args.mqtt_port);
-    mqttoptions.set_keep_alive(Duration::from_secs(args.mqtt_keep_alive.into()));
-    mqttoptions.set_transport(rumqttc::Transport::Tcp);
-
-    let (mut client, mut connection) = Client::new(mqttoptions, 10);
+    let (mut client, mut connection) =
+        Client::new(MqttOptions::new("victron", &args.host, args.port), 10);
     // this is dumb, but the mqtt client doesn't have a connect method. you need to iterate the
     // connection for it to do things. without this, nothing will happen
     thread::spawn(move || {
@@ -99,14 +100,14 @@ fn main() -> anyhow::Result<()> {
             trace!("got mqtt notification: {:?}", notification);
         }
     });
-    info!("successfully connected to mqtt broker");
 
-    if let Err(e) = client.subscribe(&args.mqtt_topic, QoS::AtLeastOnce) {
-        bail!("unable to subscribe to topic: {:?}", e);
+    info!("successfully connected to mqtt broker");
+    if let Err(e) = client.subscribe(&args.topic, QoS::AtLeastOnce) {
+        bail!("unable to subscribe to '{}'. {:?}", &args.topic, e);
     }
 
     let (sink_chan, rx) = mpsc::channel();
-    thread::spawn(move || mqtt_publisher(args.mqtt_topic.clone(), client, rx));
+    thread::spawn(move || mqtt_publisher(args.topic.clone(), client, rx));
 
     // read data from the port
     let mut buf: Vec<u8> = vec![0; 1024];
@@ -125,7 +126,7 @@ fn main() -> anyhow::Result<()> {
         }
 
         let r = r.unwrap();
-        debug!("read {} bytes from port", &r);
+        trace!("read {} bytes from port", &r);
         parser.feed(&buf[..r])?;
     }
 }
